7bf2e03711f8bbe1f893fb04e785a82c6f6b7528
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2020-12-14 20:41:29 +0530

    "Bulit in function done"

---
+++ ds.py
@@ -0,0 +1,59 @@
+# Data Sturctures
+# "Way to store and organize the data so that it can be accessed effetively" - Dictionary, Libraray
+# Built_in : List, tuple, set, Dictionary
+# User defined: Stack, queue, linked list, tree graph
+
+
+
+
+# list1 = [1,2.3,"hello",4]
+# # list1= list()
+# print(list1)
+
+# Lists are Ordered
+# list1 = [[1,2],1,2,3,4]
+# list2 = [2,1,3,4]
+
+# We can access list elements using Indexing
+# Negative Indexing in List
+# List can be nested
+# Lists are mutable
+# # Lists are Dynamic
+# print(list1)
+# print()
+# list1.append(5)
+# print(list1)
+# print()
+# del list1[0]
+# print(list1)
+
+# Store the data of different type. use indexing use negative indexing. it can be nested. it is faster then the list.
+# value of const.
+# tuple1 = (1,2,3,4,5)
+# print(type(tuple1))
+# tuple2 = 1,2,3,4,5
+# print(type(tuple2))
+# tuple3 = (2,)
+# print(type(tuple3))
+# tuple1.append(5)
+# print(tuple1)
+
+# Dictionary
+# Dictionary_name={key1:value1,key2:value2....}
+# d = {"2":{1:2}}
+# print(d)
+# d1 = {"2":2,"1":1}
+# print(d1["2"])
+# del d["2"]
+# print(d)
+# print(d1==d)
+
+# # s = set()
+# # print(s)
+# q = set([2,1,3,4])
+# for i in q:
+#     print(i)
+# # print(q)
+# # s.add(34)
+# # print(s)
+# # print(s==q)
