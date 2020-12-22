338c35812451287f5ac76aafca268bbf9e91a31b
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2020-12-22 20:49:45 +0530

    week4day02

--- 03.queue.py
+++ 03.queue.py
@@ -15,6 +15,9 @@
 # queue.append(10)
 # queue.append(20)
 # queue.append(30)
+# print(queue)
+
+
 # queue.pop(0)
 # queue.pop(0)
 # queue.pop(0)
@@ -23,6 +26,7 @@
 # queue.insert(0,10)
 # queue.insert(0,20)
 # queue.insert(0,30)
+# print(queue)
 # # queue.pop()
 # # queue.pop()
 # # queue.pop()
@@ -41,8 +45,19 @@ def dequeue():
     else:
         e = queue.pop(0)
         print("removed element:",e)
-def display()
+def display():
     print(queue)
 
 while True:
-    print("Select the operation 1. add 2.")
+    print("Select the operation 1. add 2. remove 3. show 4.quit")
+    choice = int(input())
+    if choice == 1:
+        enqueue()
+    elif choice == 2:
+        dequeue()
+    elif choice == 3:
+        display()
+    elif choice == 4:
+        break
+    else:
+        print("Enter the coreect operation")
