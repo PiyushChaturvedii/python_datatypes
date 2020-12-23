b297efb362463d5d12b18b4a106667a7df7a8277
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2020-12-23 20:37:34 +0530

    "day3"

--- 03.queue.py
+++ 03.queue.py
@@ -33,31 +33,82 @@
 # # print(not queue)
 # print(queue[0])
 
-queue=[]
-def enqueue():
-    element = input("Enter the element:")
-    queue.append(element)
-    print(element, "is added to queue!")
+# queue=[]
+# def enqueue():
+#     element = input("Enter the element:")
+#     queue.append(element)
+#     print(element, "is added to queue!")
 
-def dequeue():
-    if not queue:
-        print("queue is empty!")
-    else:
-        e = queue.pop(0)
-        print("removed element:",e)
-def display():
-    print(queue)
+# def dequeue():
+#     if not queue:
+#         print("queue is empty!")
+#     else:
+#         e = queue.pop(0)
+#         print("removed element:",e)
+# def display():
+#     print(queue)
 
-while True:
-    print("Select the operation 1. add 2. remove 3. show 4.quit")
-    choice = int(input())
-    if choice == 1:
-        enqueue()
-    elif choice == 2:
-        dequeue()
-    elif choice == 3:
-        display()
-    elif choice == 4:
-        break
-    else:
-        print("Enter the coreect operation")
+# while True:
+#     print("Select the operation 1. add 2. remove 3. show 4.quit")
+#     choice = int(input())
+#     if choice == 1:
+#         enqueue()
+#     elif choice == 2:
+#         dequeue()
+#     elif choice == 3:
+#         display()
+#     elif choice == 4:
+#         break
+#     else:
+#         print("Enter the coreect operation")
+
+# Queue Implementation using classes
+# import collections
+# q = collections.deque()
+# q.appendleft(10)
+# q.appendleft(20)
+# q.appendleft(30)
+# q.pop()
+# q.pop()
+# q.pop()
+# q.pop()
+# print(q)
+# 
+
+# import queue
+# q = queue.Queue()
+# q.put(10)
+# q.put(50)
+# q.put(30)
+# print(q.get())
+
+# Priority Queue
+# q = []
+# q.append(10)
+# q.append(40)
+# q.append(20)
+# print(q)
+# q.sort(reverse = True)
+# print(q)
+# q. pop()
+# print(q)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
