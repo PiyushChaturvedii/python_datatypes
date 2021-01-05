908ab786f80acabba9d9c1fc53383ca5fbf6a033
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-01-05 20:43:34 +0530

    day02

--- 06.Linkedlist.py
+++ 06.Linkedlist.py
@@ -6,5 +6,37 @@ class Node:
         self.ref = None
 
 class LinkedList:
-    def __int__(self):
+    def __init__(self):
         self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked list is empty!")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+    
+    def add_begin(self,data):
+        new_node=Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+    
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.ref is not None:
+                n = n.ref
+                
+
+
+LL1 = LinkedList()
+LL1.add_begin(100)
+LL1.add_begin(10)
+LL1.add_begin(20)
+LL1.print_LL()
+
