c3aaa64e3fe767627099e55a1ba7a20841186b1d
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-01-12 22:42:36 +0530

    day2

--- 06.Linkedlist.py
+++ 06.Linkedlist.py
@@ -15,7 +15,7 @@ class LinkedList:
         else:
             n = self.head
             while n is not None:
-                print(n.data)
+                print(n.data, "-->", end="")
                 n = n.ref
     
     def add_begin(self,data):
@@ -45,11 +45,78 @@ class LinkedList:
             new_node = Node(data)
             new_node.ref = n.ref
             n.ref = new_node
+        
+    def add_before(self, data, x):
+        if self.head is None:
+            print("Linked Listis empty!")
+            return
+        if self.head.data == x:
+            new_node = Node(data)
+            new_node.ref = self.head
+            self.head = new_node
+            return
+        n = self.head
+        while n.ref is not None:
+            if n.ref.data == x:
+                break
+            n = n.ref
+        if n.ref is None:
+            print ("Node is not found!")
+        else:
+            new_node = Node(data)
+            new_node.ref = n.ref
+            n.ref = new_node
+        
+    def insert_empty(self,data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+        else:
+            print("Linked listis not empty!")
+
+
+    def delete_begin(self):
+        if self.head is None:
+            print("Linked List is empty so we cant delete nodes!")
+        else:
+            self.head = self.head.ref
+
+
+    def delete_end(self):
+        if self.head is None:
+            print("LL is empty so we cant delete nodes.")
+        elif  self.head.ref is None:
+            self.head= None
+        else:
+            n = self.head
+            while n.ref.ref is not None:
+                n = n.ref
+            n.ref = None
+
+    def delete_by_value(self,x):
+        if self.head is None:
+            print("can't delete Linkedlist is empty")
+            return
+        if x == self.head.data:
+            self.head= self.head.ref
+            return
+        n = self.head
+        while n.ref is not None:
+            if x == n.ref.data:
+                break
+            n = n.ref
+        if n.ref is None:
+            print("Node is not present")
+        else:
+            n.ref = n.ref.ref
+
 
 LL1 = LinkedList()
 LL1.add_begin(10)
 LL1.add_begin(20)
-LL1.add_end(100)
-LL1.add_after(200,30)
+LL1.add_begin(30)
+# LL1.delete_end()
+# LL1.delete_begin()
+LL1.delete_by_value(10)
 LL1.print_LL()
 
