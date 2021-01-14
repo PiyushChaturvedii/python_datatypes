7cb44d782926eef1ac5bec598eb59028ab8c6050
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-01-14 20:34:55 +0530

    day03

--- 07.DoubleLinkList.py
+++ 07.DoubleLinkList.py
@@ -5,8 +5,8 @@ class Node:
         self.pref = None
 
 class doublyLL:
-    def__init__(self):
-    self.head = None
+    def __init__(self):
+        self.head = None
 
     def print_LL(self):
         if self.head is None:
@@ -14,10 +14,11 @@ class doublyLL:
         else:
             n = self.head
             while n is not None:
-                print(n.data)
+                print(n.data, "-->",end="")
                 n = n.nref
 
     def print_LL_reverse(self):
+        print()
         if self.head is None:
             print("Linked List is empty!")
         else:
@@ -25,5 +26,62 @@ class doublyLL:
             while n.nref is not None:
                 n = n.nref
             while n is not None:
-                print(n.data)
+                print(n.data,"-->",end="")
                 n = n.pref
+
+    def insert_empty(self,data):
+        if self.head is None:
+            new_node= Node(data)
+            self.head=new_node
+        else:
+            print("Linked List is not empty")
+
+    def add_begin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head= new_node
+        else:
+            new_node.nref = self.head
+            self.head.pref= new_node
+            self.head = new_node
+
+    def add_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            n.nref = new_node
+            new_node.pref = n
+
+    def add_before(self, data, x):
+        if self.head is None:
+            print("LL is empty")
+        else:
+            n = self.head
+            while n is not None:
+                if x==n.data:
+                    break
+                n = n.nref
+            if n is None:
+                print("Given value is not present in LL.")
+            else:
+                new_node = Node(data)
+                new_node.nref = n
+                new_node.pref = n.pref
+                if n.pref is not None:
+                    n.pref.nref = new_node
+                else:
+                    self.head = new_node
+                n.pref = new_node
+
+
+dl1=doublyLL()
+dl1.add_begin(10)
+# dl1.add_begin(20)
+dl1.add_end(40)
+dl1.add_before(50,40)
+dl1.print_LL()
+dl1.print_LL_reverse()
