6fc013f64bcaaf8576e7766698c11ab339ae071b
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-01-15 20:45:18 +0530

    "Day05"

--- 07.DoubleLinkList.py
+++ 07.DoubleLinkList.py
@@ -76,12 +76,58 @@ class doublyLL:
                 else:
                     self.head = new_node
                 n.pref = new_node
+    
+    def delete_begin(self):
+        print()
+        if self.head is None:
+            print("DLL is empty nothing to delete")
+            return
+        if self.head.nref is None:
+            self.head= None
+            print
+        else:
+            self.head = self.head.nref
+            self.head.pref = None
 
+    def delete_end(self):
+        if self.head is None:
+            print("LL is empty nothing to delete")
+            return
+        if self.head.nref is None:
+            self.head = None
+            print("DLL is empty after deleting the node.")
+        else:
+            n = self.head
+            while n.nref is not None:
+                n=n.nref
+            n.pref.nref = None
+
+    def delete_by_value(self, x):
+        if self.head is None:
+            print("Dll is empty can't delete")
+            return
+        if self.head.nref is None:
+            if x = self.head.data:
+                self.head = None
+            else:
+                print(" x is not present in DLL")
+            return
+        if self.head.data == x:
+            self.head = self.head.nref
+            self.head.pref = None
+            return
+        n = self.head
+        while n.nref is not None:
+            if x == n.data:
+                break
+                n= n.nref
+            
 
 dl1=doublyLL()
 dl1.add_begin(10)
-# dl1.add_begin(20)
+dl1.add_begin(20)
 dl1.add_end(40)
 dl1.add_before(50,40)
 dl1.print_LL()
-dl1.print_LL_reverse()
+
+# dl1.print_LL_reverse()
