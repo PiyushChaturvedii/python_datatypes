f0c41593e7be8ff43cd242f347f14f8f66141ed7
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-02-01 18:09:00 +0530

    day01

---
+++ Tree/tree.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,value):
+        self.left = None
+        self.data = value
+        self.right = None
+
+class Tree:
+    def createNode(self,data):
+        return Node(data)
+
+    def insert(self,node,data):
+        if node is None:
+            return self.createNode(data)
+        if data < node.data:
+            node.left = self.insert(node.left,data)
+        else:
+            node.right= self.insert(node.right,data)
+        
+        return node
+
+# Driver code
+tree = Tree()
+root=tree.createNode(5)
+print(root.data)
+tree.insert(root,2)
+tree.insert(root,10)
+tree.insert(root,7)
+tree.insert(root,15)
+tree.insert(root,12)
+tree.insert(root,20)
+tree.insert(root,30)
+tree.insert(root,6)
+tree.insert(root,8)
+
+
+
+# create a node, insert node, display, search, delete, find height of tree
+
